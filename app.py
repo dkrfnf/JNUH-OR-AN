aa62fa7d6be8c3b870807c0af681631e23412f3e
dkrfnf <dkrfnf@gmail.com>
2025-11-26 19:50:46 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -315,8 +315,8 @@ st.markdown("""
     <style>
     /* 1. 상단 여백 줄이기 (여기를 조절하세요: 1rem ~ 3rem 추천) */
     .block-container {
-        padding-top: 2rem !important; 
-        padding-bottom: 5rem !important;
+        padding-top: 1rem !important; 
+        padding-bottom: 3rem !important;
         padding-left: 1rem !important;
         padding-right: 1rem !important;
     }
